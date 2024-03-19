9587b51877d93c22116c23c060ce16b92f4a606a
Weizhe <weizhe.xu@outlook.com>
2024-03-19 15:29:58 +0800

    修改了get_answer_from_docx函数，让指令更加的模块化和定制化

--- functions.py
+++ functions.py
@@ -2,14 +2,18 @@ import os
 import re
 from docx import Document
 import pandas as pd
+import yaml
 
-def get_prompt(text,prompt):
-    return prompt.replace('xxxxxx',text)
 
-def get_prompt_baichuan(text,prompt):
+def get_prompt(text, prompt):
+    return prompt.replace('xxxxxx', text)
+
+
+def get_prompt_baichuan(text, prompt):
     messages = []
-    messages.append({"role": "user", "content": get_prompt(text,prompt)})
+    messages.append({"role": "user", "content": get_prompt(text, prompt)})
     return messages
+
 
 def extract_content_from_docx(folder_path, regex_pattern):
     extracted_contents = []
@@ -25,31 +29,44 @@ def extract_content_from_docx(folder_path, regex_pattern):
             full_text = '\n'.join([para.text for para in doc.paragraphs])
             # 使用正则表达式匹配内容
             matches = re.findall(regex_pattern, full_text, re.DOTALL)
-            matches[0] = matches[0].replace('\n','')
+            matches[0] = matches[0].replace('\n', '')
             if matches:
                 extracted_contents.extend(matches)
-            name_list.append(file.replace(".docx",""))
+            name_list.append(file.replace(".docx", ""))
     return extracted_contents, name_list
 
-def get_answer_from_docx(extracted_contents,prompt1,name_list,model,tokenizer):
+
+def get_answer_from_docx_glm(extracted_contents, prompt1, name_list, model, tokenizer):
     df_dic = {
-        "私募名称":name_list,
-        "因子组成":[],
+        "私募名称": name_list,
+        "因子组成": [],
     }
     temporary_list = []
     for text in extracted_contents:
-        response, his = model.chat(tokenizer, get_prompt(text,prompt1), history=[],temperature=0.1)
+        response, his = model.chat(tokenizer, get_prompt(text, prompt1), history=[], temperature=0.1)
         temporary_list.append(response)
         df_dic['因子组成'].append(response)
-    return pd.DataFrame(df_dic),temporary_list
+    return pd.DataFrame(df_dic), temporary_list
 
-def get_answer_from_docx_baichuan(extracted_contents,prompt,name_list,model):
+
+def get_answer_from_docx_baichuan(extracted_contents, yaml_list, name_list, model):
+    # 读取 n 个yaml_list中的元素，并创建字典
     df_dic = {
-        "私募名称":name_list,
-        "因子组成":[],
+        "私募名称": name_list
     }
+    for prompt_class in yaml_list:
+        df_dic[prompt_class] = []
+
+    # 按照 yaml_list中的要求，提取prompt并使用大语言模型
     for text in extracted_contents:
-        response = model(get_prompt_baichuan(text,prompt))
-        df_dic['因子组成'].append(response['response'])
+        for prompt_class in yaml_list:
+            prompt = get_prompt_configs(prompt_class)
+            response = model(get_prompt_baichuan(text, prompt))
+            df_dic[prompt_class].append(response['response'])
     return pd.DataFrame(df_dic)
 
+
+def get_prompt_configs(prompt_class):
+    with open('configs/prompts.yaml', 'r', encoding='utf-8') as file:
+        config = yaml.safe_load(file)
+    return config[prompt_class].replace('\n', '')
