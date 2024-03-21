d1794a5ec5a4bb970c959c0a2ca1596c260cd56d
Weizhe <weizhe.xu@outlook.com>
2024-03-21 10:34:54 +0800

    更新现在可以根据不同的标签类别，找到报告中对应的部分并输出结果

--- functions.py
+++ functions.py
@@ -4,6 +4,16 @@ from docx import Document
 import pandas as pd
 import yaml
 
+# 定义dic
+prompts_match_dic = {
+    '配置逻辑': ['因子组成', '模型种类'],
+    '公司概况': ['策略类型']
+}
+
+dic_content_extraction = {'配置逻辑': r'配置逻辑(.*?)持仓特征',
+                          '公司概况': r'公司概况(.*?)股东情况',
+                          '交易风格': r'公司概况(.*?)股东情况'}
+
 
 # 把prompt中的xxxxxx替换成尽调报告中的对应内容
 def get_prompt(text, prompt):
@@ -20,7 +30,6 @@ def get_prompt_baichuan(text, prompt):
 # 从指定目录中获取尽调报告的docx文档，然后提取出“配置逻辑”的内容
 def extract_content_from_docx(folder_path, regex_pattern):
     extracted_contents = []
-    name_list = []
     # 遍历文件夹中的所有文件
     for file in os.listdir(folder_path):
         if file.endswith(".docx"):
@@ -35,8 +44,15 @@ def extract_content_from_docx(folder_path, regex_pattern):
             matches[0] = matches[0].replace('\n', '')
             if matches:
                 extracted_contents.extend(matches)
+    return extracted_contents
+
+
+def get_name_list(folder_path):
+    name_list = []
+    for file in os.listdir(folder_path):
+        if file.endswith(".docx"):
             name_list.append(file.replace(".docx", ""))
-    return extracted_contents, name_list
+    return name_list
 
 
 def get_answer_from_docx_glm(extracted_contents, prompt1, name_list, model, tokenizer):
@@ -52,23 +68,37 @@ def get_answer_from_docx_glm(extracted_contents, prompt1, name_list, model, toke
     return pd.DataFrame(df_dic), temporary_list
 
 
-def get_answer_from_docx_baichuan(extracted_contents, yaml_list, name_list, model):
+def get_answer_template(extracted_contents, prompt, model):
+    response_list = []
+    for text in extracted_contents:
+        response = model(get_prompt_baichuan(text, prompt))
+        response_list.append(response['response'])
+    return response_list
+
+
+def get_answer_from_docx_baichuan(yaml_list, model):
+    name_list = get_name_list('尽调报告')
     # 读取 n 个yaml_list中的元素，并创建字典
     df_dic = {
         "私募名称": name_list
     }
     for prompt_class in yaml_list:
         df_dic[prompt_class] = []
-
-    # 按照 yaml_list中的要求，提取prompt并使用大语言模型
-    for text in extracted_contents:
-        for prompt_class in yaml_list:
-            prompt = get_prompt_configs(prompt_class)
-            response = model(get_prompt_baichuan(text, prompt))
-            df_dic[prompt_class].append(response['response'])
+        prompt = get_prompt_configs(prompt_class)
+        if prompt_class in prompts_match_dic['配置逻辑']:
+            # 按照 yaml_list中的要求，提取prompt并使用大语言模型
+            extracted_contents = extract_content_from_docx("尽调报告", dic_content_extraction['配置逻辑'])
+            responses = get_answer_template(extracted_contents, prompt, model)
+            df_dic[prompt_class] = responses
+        elif prompt_class in prompts_match_dic['公司概况']:
+            extracted_contents = extract_content_from_docx("尽调报告", dic_content_extraction['公司概况'])
+            responses = get_answer_template(extracted_contents, prompt, model)
+            df_dic[prompt_class] = responses
     return pd.DataFrame(df_dic)
 
 
+
+# 获取config的函数
 def get_prompt_configs(prompt_class):
     with open('configs/prompts.yaml', 'r', encoding='utf-8') as file:
         config = yaml.safe_load(file)
